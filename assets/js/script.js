eef89b5aa5982ac09afdb655f7077cd318817a64
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-04-17 13:09:23 -0500

    added moment.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,12 @@
-const submitBtn = document.getElementById('city-submit')
-const searchFormEl = document.getElementById('search-form')
+const submitBtn = document.getElementById('city-submit');
+const searchFormEl = document.getElementById('search-form');
 const userSearch = document.getElementById('user-search');
-const apiKey = 'a727a7b4e3ac0945e7318901d9d0c745'
+const apiKey = 'a727a7b4e3ac0945e7318901d9d0c745';
 
 
-var getInfo = function (event) {
+let getForecastInfo = function (event) {
     
-    const cityName = document.getElementById('city-name')
     const fiveDayCityDisplay = document.getElementById('five-day-city')
-    cityName.innerHTML = userSearch.value
     fiveDayCityDisplay.innerHTML = userSearch.value
 
     event.preventDefault();
@@ -18,15 +16,57 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+userSearch.value+'&u
 .then(response => response.json())
 .then(data =>{
     for(i = 0; i < 5; i++) {
-        document.getElementById('day' + (i+1) + 'temp').innerHTML = "Temp:" + Number(data.list[i+3].main.temp).toFixed(0) + "°";
-        document.getElementById('day' + (i+1) + 'wind').innerHTML = "Wind:" + Number(data.list[i+3].wind.speed).toFixed(0) + "mph";
-        document.getElementById('day' + (i+1) + 'hum').innerHTML = "Hum:" + Number(data.list[i].main.humidity).toFixed(0) + "%";
-        document.getElementById('icon-' + (i+1)).innerHTML = "Hum:" + Number(data.list[i+1].weather.icon);
+        document.getElementById('day' + (i+1) + 'temp').innerHTML = "Temp: " + Number(data.list[i].main.temp).toFixed(0) + "°";
+        document.getElementById('day' + (i+1) + 'wind').innerHTML = "Wind: " + Number(data.list[i].wind.speed).toFixed(0) + "mph";
+        document.getElementById('day' + (i+1) + 'hum').innerHTML = "Hum: " + Number(data.list[i].main.humidity).toFixed(0) + "%";
+        document.getElementById('icon-' + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon+".png";
+        +".png";
 
     }
 });
+
+
 }
-submitBtn.addEventListener('click', getInfo)
+
+let getcurrentInfo = function (event) {
+
+    const cityName = document.getElementById('city-name');
+    cityName.innerHTML = userSearch.value;
+    event.preventDefault();
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+userSearch.value+'&units=imperial&appid=' + apiKey)
+    .then(response => response.json())
+    .then(data =>{
+    for(i = 0; i < 5; i++) {
+        document.getElementById('single-day-temp').innerHTML = "Temp: " + Number(data.main.temp).toFixed(0) + "°";
+        document.getElementById('single-day-wind').innerHTML = "Wind: " + Number(data.wind.speed).toFixed(0) + "mph";
+        document.getElementById('single-day-hum').innerHTML = "Hum: " + Number(data.main.humidity).toFixed(0) + "%";
+        document.getElementById('single-day-icon-1').src = "http://openweathermap.org/img/wn/"+
+        data.weather[0].icon+".png";
+    }
+});
+}
+
+let importDates = function () {
+    let currentDate = document.getElementById('single-day-current');
+    currentDate.innerText = moment().format("dddd, MMMM Do YYYY");
+    var myCurrentDate=new Date();
+    var myFutureDate=new Date(myCurrentDate);
+    for (let i = 0; i < 5; i++) {
+        document.getElementById('date-' + (i+1)).innerText=moment().add((i+1), 'd').format("dddd, MMMM Do YYYY");
+
+    }
+    
+}
+
+
+
+submitBtn.addEventListener('click', getForecastInfo)
+submitBtn.addEventListener('click', getcurrentInfo)
+submitBtn.addEventListener('click', importDates)
+
+
 
 // current weather API call
 
